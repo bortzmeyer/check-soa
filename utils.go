2f610472d80b24f9126e109e5e218bc77ee8dbbc
Ollivier Robert <roberto@keltia.net>
2018-04-16 21:19:06 +0200

    Add my verbose() & debug().

--- utils.go
+++ utils.go
@@ -1,1 +1,17 @@
 package main
+
+import "log"
+
+// fDebug displays only if fDebug is set
+func debug(str string, a ...interface{}) {
+	if fDebug {
+		log.Printf(str, a...)
+	}
+}
+
+// fDebug displays only if fVerbose is set
+func verbose(str string, a ...interface{}) {
+	if !quiet {
+		log.Printf(str, a...)
+	}
+}
