8615ef90449541027bc01885b476303ef11181d5
Ollivier Robert <roberto@keltia.net>
2018-04-16 22:06:35 +0200

    -h is handled internally by "flag".

--- cli.go
+++ cli.go
@@ -21,7 +21,6 @@ var (
 	recursion      bool
 	noauthrequired bool
 	times          bool
-	help           bool
 	timeoutI       float64
 	maxTrials      int
 	nslist         map[string]nameServer
@@ -36,7 +35,6 @@ func init() {
 	}
 	flag.BoolVar(&v4only, "4", false, "Use only IPv4")
 	flag.BoolVar(&v6only, "6", false, "Use only IPv6")
-	flag.BoolVar(&help, "h", false, "Print help")
 	flag.BoolVar(&fDebug, "d", false, "Debugging")
 	flag.BoolVar(&version, "v", false, "Displays version of the code")
 	flag.BoolVar(&quiet, "q", false, "Quiet mode, display only errors")
@@ -85,9 +83,6 @@ func checkCliFlags() error {
 	timeout = time.Duration(timeoutI * float64(time.Second))
 	if maxTrials <= 0 {
 		myerror("Number of trials must be positive, not %d\n", maxTrials)
-		return ErrMustExitUsage
-	}
-	if help {
 		return ErrMustExitUsage
 	}
 	return nil
